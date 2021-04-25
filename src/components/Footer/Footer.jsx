67886504380cbf447d677f065b0b5afb05d2f3c9
Alice Ribeiro <aliceoliveiraribeiro@gmail.com>
2021-04-25 20:44:45 -0300

    feat: adicionando o componente de rodapé

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,15 @@
+import '../Footer/footer.scss';
+
+
+function Footer() {
+
+  return (
+    <footer>
+      <div className="content">
+        <p>Docket © 2019</p>
+      </div>
+    </footer>
+  )
+}
+
+export default Footer;
